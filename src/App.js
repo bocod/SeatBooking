f503558eb710e6f0782ef3a9e1a17c5130ba0349
Agustin Boccio <aboccio@hotmail.com>
2022-09-16 01:44:03 +0200

    New Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 import Picker from './components/Picker';
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header bg-dark">
+        <Header />
         <Picker />
       </header>
     </div>
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <div className="mt-1 d-flex justify-content-between align-items-center" style={{width: "240px"}}>
+            <i className="fs-4 bi bi-camera-reels"></i>
+            <h1>SeatBooking</h1>
+        </div>
+    )
+}
+
+export default Header;

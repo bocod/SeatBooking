f240fed1918ac40b84855454027221ecaf2d6efd
Agustin Boccio <aboccio@hotmail.com>
2022-09-16 00:43:45 +0200

    Clean unused code

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import 'jquery/dist/jquery.min.js';
-// import 'bootstrap/dist/js/bootstrap.min.js';
-import MoviePicker from './components/MoviePicker/MoviePicker';
-import SeatPicker from './components/SeatPicker/SeatPicker';
+import Picker from './components/Picker';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header bg-dark">
-        <MoviePicker/>
-        <SeatPicker/>
+        <Picker />
       </header>
     </div>
   );
